921934e0b8ef1e48038306399f384a8da26a6e93
leogiraldimg@gmail.com <leogiraldimg@gmail.com>
2021-07-24 17:03:52 -0300

    ✨ includes handling 404 cases

---
+++ src/components/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h4>The page you are looking for could not be found</h4>
+    <div>
+      {{ $route.params.pathMatch }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
