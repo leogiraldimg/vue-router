e688f1f475b92946f9b3de602b1bdc18d88fc773
leogiraldimg@gmail.com <leogiraldimg@gmail.com>
2021-07-24 16:45:15 -0300

    ✨ includes installing and configuring the vue router

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.vue'
 import {store} from './store'
+import router from "./router";
+
 const app  = createApp(App)
+app.use(router);
 app.use(store);
 app.mount('#app')
