8daf544d69af62ffbc9d51b52545d7a638cd83f5
leogiraldimg@gmail.com <leogiraldimg@gmail.com>
2021-07-26 08:14:29 -0300

    ✨ includes Organizing Application Layout Using Views

--- src/router.js
+++ src/router.js
@@ -2,8 +2,8 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 import Dashboard from "@/components/Dashboard.vue";
 import Tasks from "@/components/todos/TodoItems.vue";
-import Notes from "@/components/notes/Notes.vue";
 import NotFound from "@/components/NotFound.vue";
+import NotesView from "@/views/NotesView.vue";
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -11,7 +11,7 @@ const router = createRouter({
     { path: "/", component: Dashboard },
     { path: "/dashboard", component: Dashboard },
     { path: "/tasks", component: Tasks },
-    { path: "/notes", component: Notes },
+    { path: "/notes", component: NotesView },
     { path: "/:pathMatch(.*)", component: NotFound },
   ],
   linkActiveClass: "active",
---
+++ src/views/NotesView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row g-0">
+    <div class="col-2">
+      <Labels />
+    </div>
+    <div class="col-10">
+      <Notes />
+    </div>
+  </div>
+</template>
+
+<script>
+import Notes from "../components/notes/Notes.vue";
+import Labels from "../components/labels/LabelList.vue";
+
+export default {
+  components: {
+    Notes,
+    Labels,
+  },
+};
+</script>
