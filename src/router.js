3309a4e7b8a78720c749c960f2cb5167017c9ec9
leogiraldimg@gmail.com <leogiraldimg@gmail.com>
2021-08-02 08:14:24 -0300

    :sparkles: includes History Mode

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "./components/Dashboard.vue";
 import Tasks from "./components/todos/TodoItems.vue";
 import Notes from "./views/NotesView.vue";
@@ -8,7 +8,7 @@ import { store } from "./store";
 import NavbarComponent from "./components/navigation/Navbar.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
